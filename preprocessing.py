99e74ce23223be9509a3e11f00b6b68a165e94b2
BrettC-QueensU <23qnf1@queensu.ca>
2026-03-24 14:56:35 -0400

    Preprocessed Logan's Raw Data

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import glob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 # Get all CSV files from Brett Jumping Folder
 files = glob.glob('Raw_Data/Brett_RawData/*.csv')
@@ -28,7 +29,39 @@ for filepath in files:
 
     #creating a new csv file with the pre-processed data
     filename = filepath.split('_RawData/')[1].split("_RawData.csv")[0]
+
     df_processed.to_csv('Pre-Processed_Data/Brett/' + filename + '_PreProcessed.csv', index = False)
+
+
+files = glob.glob('Raw_Data/Logan_data/*/*.csv')
+
+print(files)
+
+# Preprocessing Logan Data
+for filepath in files:
+    # import csv file as data frame, and interpolate to fill any missing values
+    df = pd.read_csv(filepath)
+    df = df.interpolate(method='linear', inPlace=True)
+    df_acc = df.drop(columns='Time (s)')
+
+    # use a rolling mean on the data, with a
+    window_size = 21
+    y_sma = df_acc.rolling(window_size).mean()
+
+    # replacing the acceleration columns in the data frame with the ones in y_sma
+    df['Linear Acceleration x (m/s^2)'] = y_sma['Linear Acceleration x (m/s^2)']
+    df['Linear Acceleration y (m/s^2)'] = y_sma['Linear Acceleration y (m/s^2)']
+    df['Linear Acceleration z (m/s^2)'] = y_sma['Linear Acceleration z (m/s^2)']
+    df['Absolute acceleration (m/s^2)'] = y_sma['Absolute acceleration (m/s^2)']
+
+    # delete rows with na at the start resulting from applying sma
+    df_processed = df.dropna()
+
+    # creating a new csv file with the pre-processed data
+    filename = filepath.split('Logan_data/')[1].split("/Raw Data")[0]
+
+    df_processed.to_csv('Pre-Processed_Data/Logan/' + filename + '_PreProcessed.csv', index=False)
+
 
 #Visualisation used to aid with trial and error of window size
 #time = df['Time (s)']
