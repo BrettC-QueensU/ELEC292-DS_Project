96b6d73e6c199dd8a59b2f1646d65efc83849894
BrettC-QueensU <23qnf1@queensu.ca>
2026-03-31 14:43:56 -0400

    Create featureExtraction.py

---
+++ featureExtraction.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+# Extracts 10+ features from an inputted data-frame and normalizes data.
+# Treats the inputted df as a single window to extract data from
+# features extracted:
+def extract_features(input_df):
+
+
+
+
+
+
+
+# calls extract_features() on each df in the list, and returns them as a combined df
+def extract_features_list(input_list):
+
+
+
